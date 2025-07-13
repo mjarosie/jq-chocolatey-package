c6520dc07cd3260695b674f0a413a06763fd6b46
Maciej J <mjarosie@users.noreply.github.com>
2025-07-13 01:41:31 +0100

    update jq to 1.8.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,12 +2,12 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $fileFullPath = Join-Path $toolsDir "$packageName.exe"
-$url = 'https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-windows-i386.exe'
+$url = 'https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-windows-i386.exe'
 $checksumType = "sha256"
-$checksum = 'e4efdd6a2c463ae714ed98fd5e874fe834a3a2380e17885bd4cda1c49e5166df'
-$url64 = 'https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-windows-amd64.exe'
+$checksum = '414ec99417830178bd2f6e77fc78b34de3b12fc6b6c3229f07038c5811307124'
+$url64 = 'https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-windows-amd64.exe'
 $checksumType64 = "sha256"
-$checksum64 = '7451fbbf37feffb9bf262bd97c54f0da558c63f0748e64152dd87b0a07b6d6ab'
+$checksum64 = '23cb60a1354eed6bcc8d9b9735e8c7b388cd1fdcb75726b93bc299ef22dd9334'
 
 $packageArgs = @{
     PackageName    = $env:ChocolateyPackageName
